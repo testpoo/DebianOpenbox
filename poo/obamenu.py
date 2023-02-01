38a5d2a52bb739b21da2bcf15a296fab5ec39782
testpoo <402861037@163.com>
2023-02-01 16:01:11 +0800

    更新脚本

--- poo/obamenu.py
+++ poo/obamenu.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-# --- Start of user config ---
+# config
 applications_dirs = ("/usr/share/applications", )
-image_dir_base = "/usr/share/icons/"
-icon_Theme = "Papirus-Light/24x24"
-image_cat_prefix = "applications-"
-application_groups = ("Office", "Development", "Graphics", "Internet", "Games", "System", "Multimedia", "Utilities", "Settings")
-application_groups_zh = ("办公", "开发", "图形", "互联网", "游戏", "系统",  "多媒体", "实用工具", "设置")
-group_aliases = {"Audio":"Multimedia","AudioVideo":"Multimedia","Network":"Internet","Game":"Games", "Utility":"Utilities", "GTK":"",  "GNOME":""}
-ignoreList = ("python3.11", "feh", "pcmanfm-desktop-pref")
-simpleOBheader = False
+icon_Theme = ["/usr/share/icons/Papirus-Light/24x24/apps/",'/usr/share/icons/Papirus-Light/24x24/devices/','/usr/share/icons/Papirus-Light/24x24/actions/','/usr/share/icons/Papirus/22x22/apps/applications-']
+categories_zh = {"Settings":"设置","System":"系统","Graphics":"图形","Development":"开发","Utilities":"实用工具","Internet":"互联网","Office":"办公"}
+alias = {"Utility":"Utilities","Network":"Internet"}
+removeCats = ["XFCE","GTK","Core","X-XFCE-SettingsDialog","X-XFCE-PersonalSettings","X-LXDE-Settings","Archiving", "Compression","TerminalEmulator","FileTools","WebBrowser","TextEditor","FileManager","DesktopSettings"]
+ignoreList = ("python3.11", "feh", "pcmanfm-desktop-pref", "org.xfce.mousepad-settings")
 apps = (("终端", "terminal", "x-terminal-emulator"),("浏览器", "browser", "x-www-browser"),("文件", "system-file-manager", "pcmanfm"))
 closes = (("锁屏", "system-lock-screen", "slock"),("挂起", "system-log-out", "systemctl -i suspend",),("休眼", "system-hibernate", "systemctl -i hibernate"),("重启", "system-reboot", "systemctl -i reboot"),("关机", "system-shutdown", "systemctl -i poweroff"))
-# --- End of user config ---
 
 import glob
 
@@ -34,40 +30,23 @@ class dtItem(object):
 
   def addIcon(self, data):
     self.Icon = ""
-    di = data.strip()
-    dix = di.find("/")
-    if dix == 0:
-      self.Icon = di
-      return
-    temp_apps = glob.glob(image_dir_base + icon_Theme + "/apps/" + di + ".*")
-    temp_devices = glob.glob(image_dir_base + icon_Theme + "/devices/" + di + ".*")
-    if len(temp_apps) > 0:
-      self.Icon = temp_apps[0]
-    elif len(temp_devices) > 0:
-      self.Icon = temp_devices[0]
-    return
+    if data.find("/") == 0:
+      self.Icon = data
+    for icons in icon_Theme:
+      temp = glob.glob(icons + data + ".*")
+      if len(temp) > 0:
+        self.Icon = temp[0]
+        break
 
   def addCategories(self, data):
     self.Categories = data
 
 def getCatIcon(cat):
-  temp_apps = glob.glob(image_dir_base + icon_Theme + "/apps/" + image_cat_prefix + cat.lower() + ".*")
-  temp_actions = glob.glob(image_dir_base + icon_Theme + "/actions/" + cat.lower() + ".*")
-  if len(temp_apps) > 0:
-    return temp_apps[0]
-  elif len(temp_actions) > 0:
-    return temp_actions[0]
-  return ""
-
-def process_category(cat, curCats,  appGroups = application_groups,  aliases = group_aliases ):
-  if cat in aliases:
-    if aliases[cat] == "":
-      return ""
-    cat = aliases[cat]
-  if cat in appGroups and cat not in curCats:
-    curCats.append(cat)
-    return cat
-  return ""
+  for icons in icon_Theme:
+    temp = glob.glob(icons + cat.lower() + ".*")
+    if len(temp) > 0:
+      return temp[0]
+      break
 
 def process_dtfile(dtf,  catDict):
   active = False
@@ -83,44 +62,38 @@ def process_dtfile(dtf,  catDict):
       continue
     if l == None or len(l) < 1 or l[0] == '#':
       continue
-    if l[0] == '[' and l !=  "[Desktop Entry]":
+    if l[0]== '[' and l !=  "[Desktop Entry]":
       active = False
       continue
     eqi = l.split('=')
-    if len(eqi) < 2:
-      continue
-    if eqi[0] == "Name[zh_CN]":
+    if eqi[0] == "Name":
       this.addName(eqi[1])
-    elif eqi[0] == "Name" and this.Name == "":
+    elif eqi[0] == "Name[zh_CN]":
       this.addName(eqi[1])
     elif eqi[0] == "Exec":
       this.addExec(eqi[1])
     elif eqi[0] == "Icon":
       this.addIcon(eqi[1])
     elif eqi[0] == "Categories":
-      if eqi[1] and eqi[1][-1] == ';':
-        eqi[1] = eqi[1][0:-1]
-      cats = []
-      dtCats = eqi[1].split(';')
-      for cat in dtCats:
-        result = process_category(cat,  cats)
-      this.addCategories(cats)
-    else:
-      continue
+      dtCats = eqi[1].split(';')[0:-1]
+      this.addCategories(dtCats)
   if len(this.Categories) > 0:
     for cat in this.Categories:
-      catDict[cat].append(this)
+      if cat not in removeCats:
+        if cat in alias:
+          cat = alias[cat]
+        if cat in catDict:
+          catDict[cat].append(this)
+        else:
+          catDict[cat] = [this]
 
 categoryDict = {}
 
 if __name__ == "__main__":
-  def makeItems(name, icon_Theme, icon, command, image_dir_base):
-    return "<item label=\"" + name +"\" icon=\"" + image_dir_base + icon_Theme + "/apps/" + icon + ".svg\"><action name=\"Execute\"><execute>" + command + "</execute></action></item>"
-  def makeObItems(name, icon_Theme, icon, command, image_dir_base):
-    return "<item label=\"" + name +"\" icon=\"" + image_dir_base + icon_Theme + "/apps/" + icon + ".svg\"><action name=\"" + command + "\" /></item>"
-
-  for appGroup in application_groups:
-    categoryDict[appGroup] = []
+  def makeItems(name, icon_Theme, icon, command):
+    return "<item label=\"" + name +"\" icon=\"" + icon_Theme + icon + ".svg\"><action name=\"Execute\"><execute>" + command + "</execute></action></item>"
+  def makeObItems(name, icon_Theme, icon, command):
+    return "<item label=\"" + name +"\" icon=\"" + icon_Theme  + icon + ".svg\"><action name=\"" + command + "\" /></item>"
 
   for appDir in applications_dirs:
     appDir += "/*.desktop"
@@ -134,18 +107,18 @@ if __name__ == "__main__":
       if skipFlag == False:
         process_dtfile(dtf,  categoryDict)
 
-  if simpleOBheader == True:
-    print('<openbox_pipe_menu>')
-  else:
-    print('<?xml version="1.0" encoding="UTF-8" ?><openbox_pipe_menu xmlns="http://openbox.org/"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xsi:schemaLocation="http://openbox.org/" >')
+  print('<openbox_pipe_menu>')
 
   for app in apps:
-    print(makeItems(app[0], icon_Theme, app[1], app[2], image_dir_base))
+    print(makeItems(app[0], icon_Theme[0], app[1], app[2]))
   print("<separator />")
   for key in categoryDict:
     if categoryDict[key] == []:
       continue
-    catStr = "<menu id=\"openbox-%s\" label=\"%s\" " % (key, application_groups_zh[application_groups.index(key)])
+    if key in categories_zh:
+      catStr = "<menu id=\"openbox-%s\" label=\"%s\" " % (key, categories_zh[key])
+    else:
+      catStr = "<menu id=\"openbox-%s\" label=\"%s\" " % (key, key)
     tmp = getCatIcon(key)
     catStr += "icon=\"%s\"" % tmp
     print(catStr,  ">")
@@ -158,11 +131,11 @@ if __name__ == "__main__":
       print(progStr)
     print("</menu>")
   print("<separator />")
-  print(makeItems("配置OpenBox    ", icon_Theme, "preferences-theme", "obconf", image_dir_base))
-  print(makeObItems("重置OpenBox", icon_Theme, "systemback", "Reconfigure", image_dir_base))
-  print(makeObItems("重启OpenBox", icon_Theme, "system-reboot", "Restart", image_dir_base))
-  print(makeObItems("退出OpenBox", icon_Theme, "system-shutdown", "Exit", image_dir_base))
+  print(makeItems("配置OpenBox    ", icon_Theme[0], "preferences-theme", "obconf"))
+  print(makeObItems("重置OpenBox", icon_Theme[0], "systemback", "Reconfigure"))
+  print(makeObItems("重启OpenBox", icon_Theme[0], "system-reboot", "Restart"))
+  print(makeObItems("退出OpenBox", icon_Theme[0], "system-shutdown", "Exit"))
   print("<separator />")
   for close in closes:
-    print(makeItems(close[0], icon_Theme, close[1], close[2], image_dir_base))
+    print(makeItems(close[0], icon_Theme[0], close[1], close[2]))
   print("</openbox_pipe_menu>")
